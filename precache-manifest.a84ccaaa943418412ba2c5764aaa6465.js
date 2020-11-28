c052ac853cf2f103cf3177473be3d4039188cbf3
Vladislav <vladg782@gmail.com>
2020-11-28 13:54:58 +0700

    Updates

---
+++ precache-manifest.a84ccaaa943418412ba2c5764aaa6465.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c0b3bd2f2ac14e8c2cc1d4e03e84aff",
+    "url": "/vk-mini-app-tamagochi.github.io/index.html"
+  },
+  {
+    "revision": "9fa359b9e59fa6ba450d",
+    "url": "/vk-mini-app-tamagochi.github.io/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "18be8410eb808f07e519",
+    "url": "/vk-mini-app-tamagochi.github.io/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "9fa359b9e59fa6ba450d",
+    "url": "/vk-mini-app-tamagochi.github.io/static/js/2.6e055988.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/vk-mini-app-tamagochi.github.io/static/js/2.6e055988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18be8410eb808f07e519",
+    "url": "/vk-mini-app-tamagochi.github.io/static/js/main.e09ba86a.chunk.js"
+  },
+  {
+    "revision": "a9c0ff00cfeed7b79de8",
+    "url": "/vk-mini-app-tamagochi.github.io/static/js/runtime-main.a58e6615.js"
+  },
+  {
+    "revision": "67be67bf381bbb0fa6384218f8fdba2d",
+    "url": "/vk-mini-app-tamagochi.github.io/static/media/shit.67be67bf.png"
+  }
+]);
